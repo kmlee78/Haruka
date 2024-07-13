2c69b9ae564b3dc7346252b253b39871ae274fab
kmlee78 <jaoming787@gmail.com>
2024-07-13 21:29:44 +0900

    file change

--- src/main/java/com/programming/dmaker/constant/DMakerConstant.java
+++
@@ -1,8 +0,0 @@
-package com.programming.dmaker.constant;
-
-public class DMakerConstant {
-    public static final Integer MIN_JUNIOR_EXPERIENCE_YEAR = 1;
-    public static final Integer MAX_JUNIOR_EXPERIENCE_YEAR = 10;
-    public static final Integer MIN_SENIOR_EXPERIENCE_YEAR = 11;
-    public static final Integer MAX_SENIOR_EXPERIENCE_YEAR = 50;
-}
